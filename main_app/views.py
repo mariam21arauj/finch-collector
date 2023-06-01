4dc7cce9268ffa6e654f476b35c597512e8b7d19
Mariam Araujo <mariam21betancourt@gmail.com>
2023-05-31 21:55:19 -0700

    project part 1

---
+++ main_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# views.py
+
+# Add this cats list below the imports
+finches = [
+  {'name': 'Loki', 'species': 'Red Crossbill', 'description': 'happy a loud', 'age': 3},
+  {'name': 'Puchi', 'species': 'Evening Grosbeak', 'description': 'cute and colorful', 'age': 2},
+]
+
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
