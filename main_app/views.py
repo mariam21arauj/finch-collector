4731fa304fd08fbc00081c0acb7ba596f03c292a
Mariam Araujo <mariam21betancourt@gmail.com>
2023-06-04 18:48:30 -0700

    finishes lab part 2

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,5 @@
 from django.shortcuts import render
-
-# views.py
-
-# Add this cats list below the imports
-finches = [
-  {'name': 'Loki', 'species': 'Red Crossbill', 'description': 'happy and loud', 'age': 3},
-  {'name': 'Puchi', 'species': 'Evening Grosbeak', 'description': 'cute and colorful', 'age': 2},
-]
+from .models import Finch
 
 
 def home(request):
@@ -16,6 +9,10 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
-  return render(request, 'finches/index.html', {
-    'finches': finches
-  })
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', {'finches': finches})
+
+def finches_detail(request, finch_id):
+   finch = Finch.objects.get(id=finch_id)
+   return render(request, 'finches/detail.html', { 'finch': finch })
+
